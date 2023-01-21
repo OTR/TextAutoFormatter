8f2ac5185ada14d2f09c35c7be7f400a0816aad8
OTR <49165533+OTR@users.noreply.github.com>
2023-01-21 13:33:44 +0600

    [CHORE] Create emply project with IntelliJ Build System

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+// This is a studying project in Kotlin. This is a program that takes some piece of a text and makes some predefined
+// formatting to it. Such as trims extra spaces, puts a dot at the end of a sentence, puts a tab symbol
+// before a paragraph and so on. Makes some addition measurement such as counts symbols, words, sentences, paragraphs.
+fun main(args: Array<String>) {
+    println("Hello World!")
+    println("Program arguments: ${args.joinToString()}")
+}
