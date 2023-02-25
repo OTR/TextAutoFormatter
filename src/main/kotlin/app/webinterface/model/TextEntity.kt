6763fd280e60bc30ec1e48867395de501929caed
OTR <49165533+OTR@users.noreply.github.com>
2023-02-25 21:28:11 +0600

    [FIX] Change TextEntity constructor

--- src/main/kotlin/app/webinterface/model/TextEntity.kt
+++ src/main/kotlin/app/webinterface/model/TextEntity.kt
@@ -1,19 +1,42 @@
 package app.webinterface.model
 
-class TextEntity(
-    override val id: Int,
-    override val unformattedText: String,
-    override val stageOneText: String,
-    override val handFormattedText: String,
-    override val posted: Boolean
-): ITextEntity {
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
 
+@Entity
+class TextEntity(): ITextEntity {
+
+    constructor(id: Int, unformattedText: String): this() {
+        this.id = id
+        this.unformattedText = unformattedText
+    }
+
+    constructor(
+        id: Int, unformattedText: String, stageOneText: String?,
+        handFormattedText: String?, posted: Boolean?
+    ): this() {
+        this.id = id
+        this.unformattedText = unformattedText
+        this.stageOneText = stageOneText
+        this.handFormattedText = handFormattedText
+        this.posted = posted
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    override var id: Int = 0
+    override var unformattedText: String = ""
+    override var stageOneText: String? = null
+    override var handFormattedText: String? = null
+    override var posted: Boolean? = null
 }
 
 interface ITextEntity {
     val id: Int
     val unformattedText: String
-    val stageOneText: String
-    val handFormattedText: String
-    val posted: Boolean
+    val stageOneText: String?
+    val handFormattedText: String?
+    val posted: Boolean?
 }
--- src/test/kotlin/app/webinterface/model/TextEntityTests.kt
+++ src/test/kotlin/app/webinterface/model/TextEntityTests.kt
@@ -5,7 +5,6 @@ import org.hamcrest.Matchers.hasProperty
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import app.webinterface.model.TextEntity
 
 /**
  * This test class holds tests for data model `TextEntity.kt`
@@ -13,14 +12,14 @@ import app.webinterface.model.TextEntity
  *   id: Int | INT | PRIMARY KEY AUTOINCREMENT FROM 1 to MAXINT
  *   unformattedText: String | VARCHAR(255) NOT NULL | initial (raw) text without
  *     formatting
- *   stageOneText: String | VARCHAR(255) NULL | text after execution of auto
+ *   stageOneText: String? | VARCHAR(255) NULL | text after execution of auto
  *     formatting function
- *   handFormatterText: String | VARCHAR(255) NULL | user corrected text.
+ *   handFormatterText: String? | VARCHAR(255) NULL | user corrected text.
  *     How the user thinks final text should look like. It's needed to run test
  *     against expected (handFormattedText) and actual (stageOneText) to measure
  *     accuracy of auto formatting function and continue improving that function
  *     if tests not passed
- *  posted: boolean | BOOLEAN | Is this text already in use
+ *  posted: Boolean? | BOOLEAN NULL | Is this text already in use
  */
 class TextEntityTests {
 
@@ -28,7 +27,7 @@ class TextEntityTests {
     @DisplayName("Text Entity has certain fields")
     fun testTextEntityHasCertainFields() {
 
-        val textEntity: TextEntity = TextEntity(1, "","","",true)
+        val textEntity: TextEntity = TextEntity(1, "", "", "", true)
         assertThat(textEntity, hasProperty("id"))
         assertThat(textEntity, hasProperty("unformattedText"))
         assertThat(textEntity, hasProperty("stageOneText"))
@@ -40,5 +39,44 @@ class TextEntityTests {
         assertTrue(textEntity.stageOneText is String)
         assertTrue(textEntity.handFormattedText is String)
         assertTrue(textEntity.posted is Boolean)
+    }
+
+    /**
+     * Being able to create Text Entity with minimum arguments provided
+     * Required arguments are `id` and `unformattedText`
+     * Other arguments are optional
+     */
+    @Test
+    @DisplayName("Create Text Entity with minimal args")
+    fun testTextEntityWithMinArgs() {
+        val textEntity: TextEntity = TextEntity(1, "Hello")
+        assertThat(textEntity, hasProperty("id"))
+        assertThat(textEntity, hasProperty("unformattedText"))
+        assertThat(textEntity, hasProperty("stageOneText"))
+        assertThat(textEntity, hasProperty("handFormattedText"))
+        assertThat(textEntity, hasProperty("posted"))
+
+        assertTrue(textEntity.id is Int)
+        assertTrue(textEntity.unformattedText is String)
+        assertTrue(textEntity.stageOneText == null)
+        assertTrue(textEntity.handFormattedText == null)
+        assertTrue(textEntity.posted == null)
+    }
+
+    @Test
+    @DisplayName("Create Text Entity with null args")
+    fun testTextEntityWithNullArgs() {
+        val textEntity: TextEntity = TextEntity(1, "Hello", null, null, null)
+        assertThat(textEntity, hasProperty("id"))
+        assertThat(textEntity, hasProperty("unformattedText"))
+        assertThat(textEntity, hasProperty("stageOneText"))
+        assertThat(textEntity, hasProperty("handFormattedText"))
+        assertThat(textEntity, hasProperty("posted"))
+
+        assertTrue(textEntity.id is Int)
+        assertTrue(textEntity.unformattedText is String)
+        assertTrue(textEntity.stageOneText == null)
+        assertTrue(textEntity.handFormattedText == null)
+        assertTrue(textEntity.posted == null)
     }
 }
