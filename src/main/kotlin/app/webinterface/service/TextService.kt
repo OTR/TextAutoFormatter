e7a0504f5325396fe2f8294995f3a6d5604283f1
OTR <49165533+OTR@users.noreply.github.com>
2023-02-25 21:41:04 +0600

    [TEST] Add tests for service layer

---
+++ src/main/kotlin/app/webinterface/service/TextService.kt
@@ -0,0 +1,51 @@
+package app.webinterface.service
+
+import org.springframework.stereotype.Service
+import app.webinterface.model.TextEntity
+import app.webinterface.repository.TextRepository
+
+/**
+ * Text Service should provide 5 basic CRUD functions
+ *   - create a new text entity
+ *   - get a text entity by ID
+ *   - get all the text entities
+ *   - update a text entity by ID
+ *   - delete a text entity
+ */
+interface ITextService {
+
+    fun create(testEntity: TextEntity): TextEntity
+
+    fun getById(id: Int): TextEntity
+
+    fun getAll(): List<TextEntity>
+
+    fun updateById(id: Int, updatedTextEntity: TextEntity): TextEntity
+
+    fun deleteById(id: Int): Unit
+}
+
+@Service
+class TextService(var textRepository: TextRepository): ITextService {
+    override fun create(testEntity: TextEntity): TextEntity {
+        return textRepository.save(testEntity)
+    }
+
+    override fun getById(id: Int): TextEntity {
+        return textRepository.getReferenceById(id)
+    }
+
+    override fun getAll(): List<TextEntity> {
+        return textRepository.findAll()
+    }
+
+    override fun updateById(id: Int, updatedTextEntity: TextEntity): TextEntity {
+        updatedTextEntity.id = id
+        return textRepository.save(updatedTextEntity)
+    }
+
+    override fun deleteById(id: Int) {
+        return textRepository.deleteById(id)
+    }
+
+}
